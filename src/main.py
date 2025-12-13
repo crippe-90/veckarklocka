d95cd9ac5a7e806a8d703c25224a805bcd646ba4
crippe-90 <christoffernorell@yahoo.se>
2025-12-13 07:53:36 +0100

    Added possibility to specify path

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
-from datechecker import is_nth_weekday_now
 import tomllib
+import sys
 
+from datechecker import is_nth_weekday_now
 from constants import WEEKDAYS
 from send_email import send_emails
 
 if __name__=="__main__":
-    with open("config.toml", "rb") as f:
+    config_path = "config.toml"
+    if len(sys.argv) > 1 and sys.argv[1] and sys.argv[1].startswith("config="):
+        arg = sys.argv[1]
+        config_path = arg.split('=')[1]
+    
+    with open(config_path, "rb") as f:
         config = tomllib.load(f)
         day = config["appsettings"]["DAY"]
         nth = config["appsettings"]["NTH"]
@@ -13,3 +19,4 @@ if __name__=="__main__":
         
         if is_nth_weekday_now(nth,weekday):
             send_emails(f"Today is the {nth} {day} of the month!")
+            
